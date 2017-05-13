a6c0dc845b9754eec44bf15139795c8e64b8c15f
Chess Seventh <chess.seventh@gmail.com>
2017-05-13 13:31:09 +0200

    working bot - simple but no data showing yet

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 import time
 import sys
 import logging as lg
-import telegram as Tg
+#import telegram as Tg
 from telegram.ext import Updater, CommandHandler
 from constants import BOT_API_TOKEN
 
@@ -16,33 +16,35 @@ def reboot_bot(update):
     time.sleep(0.5)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-def add_commands(my_updater, name, func):
-    """ Add commands the bot """
-    my_updater.dispatcher.add_handler(CommandHandler(name, func))
-
-def start_bot(myupdater):
-    """ Start the bot """
-    #bot.send_message(chat_id=update.message.chat_id, text="Your Hackathon bot, talk!")
-    myupdater.start_webhook('', 80, 'urlpath')
-    myupdater.start_polling()
-
 def teamday(bot, update):
     """ Team score of a team for the given day """
-    print("hello")
+    update.message.reply_text('hello show data of teamday here')
 
 def teammonth(bot, update):
     """ Team score of a team for the current month """
-    print("hello")
+    update.message.reply_text('hello show data of teammonth here')
 
 def globalmonth(bot, update):
     """ Overall scores for the current month """
-    print("hello")
+    update.message.reply_text('hello show data of global month  here')
 
 def globalday(bot, update):
     """ Overall scores for the current day """
-    print("hello")
+    update.message.reply_text('hello show data of global day  here')
 
 
 updater = Updater(BOT_API_TOKEN)
-mybot = Tg.Bot(BOT_API_TOKEN)
+# mybot = Tg.Bot(BOT_API_TOKEN)
+#add_commands(updater, 'teamday', 'teamday')
+#add_commands(updater, 'teammonth', 'teammonth')
+#add_commands(updater, 'globalmonth', 'globalmonth')
+#add_commands(updater, 'globalday', 'globalday')
+
+updater.dispatcher.add_handler(CommandHandler('teamday', teamday))
+updater.dispatcher.add_handler(CommandHandler('teammonth', teammonth))
+updater.dispatcher.add_handler(CommandHandler('globalmonth', globalmonth))
+updater.dispatcher.add_handler(CommandHandler('globalday', globalday))
+
 lg.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=lg.INFO)
+updater.start_polling()
+updater.idle()
